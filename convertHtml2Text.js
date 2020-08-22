72ed902340ee99a34f0ccadd65914e67d6725ebc
SOKO <sackso@naver.com>
2020-08-22 20:35:41 +0900

    dir 변경시 전부 변경하도록 수정

--- convertHtml2Text.js
+++ convertHtml2Text.js
@@ -6,7 +6,7 @@ var iconv = require('iconv-lite');
 var dirTree = require('directory-tree');
 
 
-var pDir = '/media/sackso/1T_400G/LV1_work/30.jwfreenote/jwfreenote/BookData/';
+var pDir = '/media/sackso/1T_400G/LV1_work/30.jwfreenote/jwfreenote/BookData/Project/90.개인사';
 
 fnFindDirList(pDir);
 
@@ -21,22 +21,20 @@ function fnFindDirList(dirName){
                 console.log(dirName + '/'+element + ' is directory');
                 fnFindDirList(dirName + '/'+element);
             }else{//
-                console.log(dirName + '/'+element + ' is file');
+                if(element.indexOf('htm') > -1){
+                    console.log(dirName + '/'+element + ' is file');
+                    fnHtm2Md(dirName,element);
+                }
             }
         });
     });
 }
 
-
-
-
-
-
+//파일을 읽어 데이터로 출력, 태그 제거
 function fnHtm2Md(currDir, srcFileName){
-    //파일을 읽어 데이터로 출력, 태그 제거
-
     var strFileData = '';
-    strFile = fs.readFileSync(currDir +'/' +srcFilePath+'.html');
+    var strNewFileName = srcFileName.replace(/\.htm/,'\.md');
+    strFile = fs.readFileSync(currDir +'/' +srcFileName);
 
     //encoding
     var utf8Str = iconv.decode(strFile,'euc-kr');
@@ -45,10 +43,10 @@ function fnHtm2Md(currDir, srcFileName){
     strFileData = utf8Str.replace(/(<([^>]+)>)/gi, "");
     strFileData = strFileData.replace(/&nbsp;/gi,' ');
     
-    console.log(strFileData);
+    //console.log(strFileData);
 
     //읽은 데이터를 파일로 저장
-    fs.writeFileSync(currDir + '/'+srcFileName+'.md',strFileData,'utf-8');
+    fs.writeFileSync(currDir + '/'+strNewFileName,strFileData,'utf-8');
 
     
 
